81db963c0e9c578b3d42dfb0907d12c4b0841ac1
ngochoangit2k1 <59520070+ngochoangit2k1@users.noreply.github.com>
2021-10-10 00:49:40 +0700

    1

--- node-express-boilerplate-master/src/controllers/index.js
+++
@@ -1,4 +0,0 @@
-module.exports.authController = require('./auth.controller');
-module.exports.userController = require('./user.controller');
-module.exports.studentController = require('./student.controller');
-module.exports.classController = require('./class.controller');
--- node-express-boilerplate-master/src/validations/index.js
+++
@@ -1,4 +0,0 @@
-module.exports.authValidation = require('./auth.validation');
-module.exports.userValidation = require('./user.validation');
-module.exports.studentValidation = require('./sinhvien.validation');
-module.exports.classValidation = require('./class.validation');
